327c20aa773e65164ac889ba296d78b5f7564d10
Igor Lemes <51129898+Lemesigor@users.noreply.github.com>
2021-12-27 19:17:04 -0300

    fix wsl dotfiles commands

--- Debian/WSL2/initial_setup.sh
+++ Debian/WSL2/initial_setup.sh
@@ -9,18 +9,11 @@ sudo apt-get update &&
 
 ## Donwload node repo
 curl -sL https://deb.nodesource.com/setup_14.x | sudo bash - &&
-cat /etc/apt/sources.list.d/nodesource.list &&
-deb https://deb.nodesource.com/node_14.x focal main &&
-deb-src https://deb.nodesource.com/node_14.x focal main &&
 #####
 
 
 ## Install docker and compose
-sudo apt-get install \
-    ca-certificates \
-    curl \
-    gnupg \
-    lsb-release &&
+sudo apt-get install ca-certificates curl gnupg lsb-release -y &&
 
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg &&
 
@@ -29,28 +22,28 @@ echo \
   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null &&
 
 sudo apt-get update &&
-sudo apt-get install docker-ce docker-ce-cli containerd.io &&
+sudo apt-get install docker-ce docker-ce-cli containerd.io -y &&
 
 sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose &&
 sudo chmod +x /usr/local/bin/docker-compose &&
 
-#######
+######
 
-## Default apps 
-sudo apt install zsh python3 python-pip nodejs -y &&
+# Default apps 
+sudo apt install zsh python3 python3-pip nodejs -y &&
 
-## ohmyzsh
-wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh &&
+# ohmyzsh
+sudo sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" &&
 
 ## Autocomplete and syntax highlight plugins
-git clone https://github.com/zsh-users/zsh-autosuggestions.git $ZSH_CUSTOM/plugins/zsh-autosuggestions &&
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting &&
+sudo git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions &&
+sudo git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting &&
 
-## rm -rf ~/.zshrc &&
+ rm -rf ~/.zshrc &&
 
 git clone https://github.com/Lemesigor/dotfiles.git ~/.dotfiles &&
 cd ~/.dotfiles &&
-## ln -s ~/.dotfiles/Debian/.zshrc ~/.zshrc &&
+ln -s ~/.dotfiles/Debian/.zshrc ~/.zshrc &&
 ln -s ~/.dotfiles/.gitconfig ~/.gitconfig &&
 
 chsh -s $(which zsh) &&
