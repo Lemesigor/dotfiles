2647708a071052d8a86d7b662bcfac3c61dbbe3e
Igor  Lemes <51129898+Lemesigor@users.noreply.github.com>
2021-12-27 22:30:03 -0300

    Update initial setup in mac

--- MacOS/initial_setup.sh
+++ MacOS/initial_setup.sh
@@ -1,20 +1,20 @@
 #!/bin/sh
 
 # Initial CLI
-xcode-select --install
+xcode-select --install &&
 
 ### Oh My ZSH
-sudo sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
-rm -rf ~/.zshrc
+sudo sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" &&
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting &&
+rm -rf ~/.zshrc &&
 
 ### Creating the dotfiles links
-git clone https://github.com/Lemesigor/dotfiles.git ~/.dotfiles
-cd ~/.dotfiles
-ln -s ~/.dotfiles/MacOS/.zshrc ~/.zshrc
-ln -s ~/.dotfiles/.gitconfig ~/.gitconfig
+git clone git@github.com:Lemesigor/dotfiles.git ~/.dotfiles &&
+cd ~/.dotfiles &&
+ln -s ~/.dotfiles/MacOS/.zshrc ~/.zshrc &&
+ln -s ~/.dotfiles/.gitconfig ~/.gitconfig &&
 
 ### Homebrew installation
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> /Users/igorgomeslemes/.profile && eval "$(/opt/homebrew/bin/brew shellenv)"
-brew bundle --file ~/.dotfiles/MacOS/Brewfile
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)" &&
+echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> /Users/igorgomeslemes/.profile && eval "$(/opt/homebrew/bin/brew shellenv)" &&
+brew bundle --file ~/.dotfiles/MacOS/Brewfile &&
